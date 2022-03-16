473216a44875cac22edbd4600e3263a2d28c5860
Ozypher <47003261+Ozypher@users.noreply.github.com>
2022-03-15 21:49:35 -0400

    Current API options

--- MovieShopAPI/Controllers/MoviesController.cs
+++ MovieShopAPI/Controllers/MoviesController.cs
@@ -26,4 +26,29 @@ public class MoviesController : ControllerBase
 
         return Ok(movie);
     }
+    [Route("top-grossing")]
+    [HttpGet]
+    public async Task<IActionResult> GetTop30Grossing()
+    {
+        var moviemodel = await _movieService.GetTop30GrossingMovies();
+        if (moviemodel == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(moviemodel);
+    }
+
+    [Route("genre/{genreId}")]
+    [HttpGet]
+    public async Task<IActionResult> GetGenreWithPagination(int genreId)
+    {
+        var movies = await _movieService.GetMoviesByGenrePagination(genreId);
+        if (movies == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(movies);
+    }
 }
